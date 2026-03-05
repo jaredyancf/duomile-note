a299b4899a393963917dc007bff84d2b5704f2ef
jaredyancf <768188869@qq.com>
2026-03-05 18:57:37 +0800

    2026-3-5更新

--- 周四晚上7点-高崎C++/2026-3-5/note.cpp
+++ 周四晚上7点-高崎C++/2026-3-5/note.cpp
@@ -1,4 +1,29 @@
 //   出勤:  陶源
 
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	
+	int   n;
+	cin>>n;
+	int g=n%10,   s=n/10%10,b=n/100%10,  q=n/1000;
+	
+	cout<<b*1000+q*100+g*10+s;
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 //oj.czos.cn
 ////   1612    1613  1614 1615   1616   1617  1618  1619   
