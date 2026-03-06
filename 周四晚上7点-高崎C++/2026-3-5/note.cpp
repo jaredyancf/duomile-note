72d76a9adbb5c4ae34c376362ed3b2c701d1187e
jaredyancf <768188869@qq.com>
2026-03-06 17:48:31 +0800

    更新

--- 周四晚上7点-高崎C++/2026-3-5/note.cpp
+++ 周四晚上7点-高崎C++/2026-3-5/note.cpp
@@ -3,12 +3,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+	//1619
+	//~ int   n;
+	//~ cin>>n;
+	//~ int g=n%10,   s=n/10%10,b=n/100%10,  q=n/1000;
 	
-	int   n;
+	//~ cout<<b*1000+q*100+g*10+s;
+	
+	
+	
+	//1618
+	//~ int  n;
+	//~ cin>>n;
+	//~ int  d2=n/100,d3=n%100;
+	//~ float pj=(n+d2+d3)/3.0;
+	//~ cout<<fixed<<setprecision(2)<<pj;
+
+	
+	
+	
+	
+	
+	//1617
+	int n;
 	cin>>n;
-	int g=n%10,   s=n/10%10,b=n/100%10,  q=n/1000;
+	int  d2=  n/1%10+n/10%10+n/100%10+n/1000%10;
+	cout<<n+d2;
 	
-	cout<<b*1000+q*100+g*10+s;
+	
+	
+	
 	
 	return 0;
 }
